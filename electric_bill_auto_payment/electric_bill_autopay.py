1c48213bd764f104346ad92f79bf36c213050e9a
Vishal Shah <vish2398@gmail.com>
2018-11-15 13:39:00 -0500

    updating script as the website was redesigned

--- electric_bill_auto_payment/electric_bill_autopay.py
+++ electric_bill_auto_payment/electric_bill_autopay.py
@@ -1,5 +1,6 @@
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 import configparser
 
 config = configparser.ConfigParser()
@@ -32,43 +33,52 @@ browser.find_element_by_name('ctl04$login$ctl00$txtPassword').send_keys(config.g
 #click login button to login to the site.
 browser.find_element_by_name("ctl04$login$ctl00$btnLogin").click()
 
-#once logged in, click the Pay now button and that will bring up a new pop up to submit payment
-browser.find_element_by_name('ctl04$Menu1$ctl00$btnPay').click()
+#navigate to the pay bill link
+browser.get('https://www.starnik.net/UtilityTrakR/UT3/Current/RP_PayBill.aspx');
 
-#switch control to the popup window and thus the browser var has a reference to that dom.
-browser.switch_to_window(browser.window_handles[1])
+#click continue on the payment summary
+browser.find_element_by_name('ctl04$ctl01$btnSelectAccount').click()
 
-#browser.find_elements_by_css("input[type='radio'][value='rdoCheckingAccount']").click
-browser.find_element_by_id('ctl04_ctl00_rdoCheckingAccount').click()
-#give a little time for the click.  might not need it, but just so we don't get not found as the other elements come into visibility
-sleep(1)
-browser.find_element_by_name('ctl04$ctl00$txtBankName').send_keys(config.get("Electric_Bill","bankName"))
-browser.find_element_by_name('ctl04$ctl00$txtAccountName').send_keys(config.get("Electric_Bill","accountName"))
-browser.find_element_by_name('ctl04$ctl00$txtBillingFirstName').send_keys(config.get("Electric_Bill","firstName"))
-browser.find_element_by_name('ctl04$ctl00$txtBillingLastName').send_keys(config.get("Electric_Bill","lastName"))
-browser.find_element_by_id('ctl04_ctl00_chkSameAddress').click()
-browser.find_element_by_name('ctl04$ctl00$txtBillingPhone').send_keys(config.get("Electric_Bill","phone"))
-browser.find_element_by_name('ctl04$ctl00$txtEmail').send_keys(config.get("Electric_Bill","email"))
-#routing
-browser.find_element_by_name('ctl04$ctl00$txtABACode').send_keys(config.get("Electric_Bill","routing"))
-browser.find_element_by_name('ctl04$ctl00$txtConfirmABACode').send_keys(config.get("Electric_Bill","routing"))
+#click on ECheck
+browser.find_element_by_id('ctl04_ctl01_rblOnlinePaymentTypeGroupID_1').click()
+
+browser.find_element_by_name('ctl04$ctl01$txtBankName').send_keys(config.get("Electric_Bill","bankName"))
+browser.find_element_by_name('ctl04$ctl01$txtAccountName').send_keys(config.get("Electric_Bill","accountName"))
+
+#routing number and confirm routing number
+browser.find_element_by_name('ctl04$ctl01$txtABACode').send_keys(config.get("Electric_Bill","routing"))
+browser.find_element_by_name('ctl04$ctl01$txtConfirmABACode').send_keys(config.get("Electric_Bill","routing"))
 
 #acct num
-browser.find_element_by_name('ctl04$ctl00$txtAccountNumber').send_keys(config.get("Electric_Bill","account"))
-browser.find_element_by_name('ctl04$ctl00$txtConfirmAccountNumber').send_keys(config.get("Electric_Bill","account"))
+browser.find_element_by_name('ctl04$ctl01$txtAccountNumber').send_keys(config.get("Electric_Bill","account"))
+browser.find_element_by_name('ctl04$ctl01$txtConfirmAccountNumber').send_keys(config.get("Electric_Bill","account"))
+
+browser.find_element_by_name('ctl04$ctl01$txtBillingFirstName').send_keys(config.get("Electric_Bill","firstName"))
+browser.find_element_by_name('ctl04$ctl01$txtBillingLastName').send_keys(config.get("Electric_Bill","lastName"))
+
+
+#street address
+browser.find_element_by_name('ctl04$ctl01$txtBillingAddress').send_keys(config.get("Electric_Bill","streetAddress"))
+browser.find_element_by_name('ctl04$ctl01$txtBillingCity').send_keys(config.get("Electric_Bill","city"))
+
+stateSelect = Select(browser.find_element_by_id("ctl04_ctl01_ddlBillingStateID"))
+stateSelect.select_by_value(config.get("Electric_Bill","state"))
+
+browser.find_element_by_name('ctl04$ctl01$txtBillingZipCode').send_keys(config.get("Electric_Bill","zip"))
+
 
 #click continue
-browser.find_element_by_name('ctl04$ctl00$btnSubmit').click()
+browser.find_element_by_name('ctl04$ctl01$btnSubmit').click()
 
 #accepts the alert box popup.
 browser.switch_to_alert().accept()
 
 #click the i agree
-browser.find_element_by_name('ctl04$ctl00$chkAgree').click()
+browser.find_element_by_name('ctl04$ctl01$chkAgree').click()
 
 #confirm the payment.  Sleep 30 seconds before doing so so that I can review.
-sleep(30)
-browser.find_element_by_name('ctl04$ctl00$btnConfirm').click()
+sleep(15)
+browser.find_element_by_name('ctl04$ctl01$btnConfirm').click()
 
 #accepts the alert box popup.
 browser.switch_to_alert().accept()
@@ -77,13 +87,13 @@ browser.switch_to_alert().accept()
 #browser.close()
 
 #now switch back to the main window and log out and close the main window
-browser.switch_to_window(browser.window_handles[0])
+#browser.switch_to_window(browser.window_handles[0])
 
 #review 
 sleep(10)
 
 #log out
-browser.find_element_by_id('menu2').click()
+browser.find_element_by_id('ctl04$ctl00$btnLogout').click()
 
 #close main window
 browser.close()
